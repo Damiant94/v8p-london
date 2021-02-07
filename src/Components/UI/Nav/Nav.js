883db5632cce054989dc7f0175f0a57cc1c036f3
Damiant94 <damian.tulacz@gmail.com>
2021-02-07 01:22:08 +0100

    added active links style for navbar

--- src/Components/UI/Nav/Nav.js
+++ src/Components/UI/Nav/Nav.js
@@ -6,22 +6,22 @@ const nav = () => {
     return (
         <nav className={classes.Nav}>
             <ul>
-                <li><NavLink to="/Home">Home</NavLink></li>
-                <li><NavLink to="/About-us">About Us</NavLink></li>
+                <li><NavLink activeClassName={classes.active} to="/Home">Home</NavLink></li>
+                <li><NavLink activeClassName={classes.active} to="/About-us">About Us</NavLink></li>
                 <li>
-                    <NavLink to="/Our-service">Our Service</NavLink>
+                    <NavLink activeClassName={classes.active} to="/Our-service">Our Service</NavLink>
                     <div className={classes.OurServiceSubmenu}>
                         <ul>
-                            <li><NavLink to="/Bodywork">Bodywork</NavLink></li>
-                            <li><NavLink to="/Repairs">Repairs</NavLink></li>
-                            <li><NavLink to="/Servicing">Servicing</NavLink></li>
-                            <li><NavLink to="/Tuning">Tuning</NavLink></li>
+                            <li><NavLink activeClassName={classes.active} to="/Bodywork">Bodywork</NavLink></li>
+                            <li><NavLink activeClassName={classes.active} to="/Repairs">Repairs</NavLink></li>
+                            <li><NavLink activeClassName={classes.active} to="/Servicing">Servicing</NavLink></li>
+                            <li><NavLink activeClassName={classes.active} to="/Tuning">Tuning</NavLink></li>
                         </ul>
                     </div>
                 </li>
-                <li><NavLink to="/Special-offers">Special Offers</NavLink></li>
-                <li><NavLink to="/Gallery">Gallery</NavLink></li>
-                <li><NavLink to="/Contact">Contact</NavLink></li>
+                <li><NavLink activeClassName={classes.active} to="/Special-offers">Special Offers</NavLink></li>
+                <li><NavLink activeClassName={classes.active} to="/Gallery">Gallery</NavLink></li>
+                <li><NavLink activeClassName={classes.active} to="/Contact">Contact</NavLink></li>
             </ul>
         </nav>
     )
